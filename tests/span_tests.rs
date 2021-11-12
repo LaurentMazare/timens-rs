fc4dea8da2bb1d759caa24045597953b4ab3a1eb
laurent <laurent.mazare@gmail.com>
2021-11-12 22:40:37 +0000

    Tweak the tests.

--- tests/span_tests.rs
+++ tests/span_tests.rs
@@ -1,56 +1,42 @@
 use timens::SpanNs;
 
+fn test_str(s: SpanNs, str: &str) {
+    assert_eq!(s.to_string(), str)
+}
+
 #[test]
-fn roundtrip_sexp() {
-    assert_eq!(SpanNs::ZERO.to_string(), "0s");
-    assert_eq!(SpanNs::SEC.to_string(), "1s");
-    assert_eq!((SpanNs::MS * 1.).to_string(), "1ms");
-    assert_eq!((SpanNs::MS * 2.).to_string(), "2ms");
-    assert_eq!((SpanNs::MS * 222.).to_string(), "222ms");
-    assert_eq!((SpanNs::MS * 222.222).to_string(), "222.222ms");
-    assert_eq!((SpanNs::MS * 1.5).to_string(), "1.5ms");
-    assert_eq!((SpanNs::MS * 1.2).to_string(), "1.2ms");
-    assert_eq!((SpanNs::MS * 1.23).to_string(), "1.23ms");
-    assert_eq!((SpanNs::MS * 1.234).to_string(), "1.234ms");
-    assert_eq!((SpanNs::MS * 1.2345).to_string(), "1.2345ms");
-    assert_eq!((SpanNs::MS * 1.23456).to_string(), "1.23456ms");
-    assert_eq!((SpanNs::MS * 1.234567).to_string(), "1.234567ms");
-    assert_eq!((SpanNs::MS * 0.5).to_string(), "500us");
-    assert_eq!(SpanNs::US.to_string(), "1us");
-    assert_eq!(SpanNs::NS.to_string(), "1ns");
-    assert_eq!(SpanNs::of_day(12.).to_string(), "12d");
-    assert_eq!((SpanNs::DAY * 12 + SpanNs::NS).to_string(), "12d1ns");
-    assert_eq!((SpanNs::DAY * 12 + SpanNs::NS).neg().to_string(), "-12d1ns");
-    assert_eq!(
-        (SpanNs::DAY * 12 + SpanNs::MS + SpanNs::NS).to_string(),
-        "12d1.000001ms"
+fn to_string() {
+    test_str(SpanNs::ZERO, "0s");
+    test_str(SpanNs::SEC, "1s");
+    test_str(SpanNs::MS * 1., "1ms");
+    test_str(SpanNs::MS * 2., "2ms");
+    test_str(SpanNs::MS * 222., "222ms");
+    test_str(SpanNs::MS * 222.002, "222.002ms");
+    test_str(SpanNs::MS * 222.022, "222.022ms");
+    test_str(SpanNs::MS * 222.222, "222.222ms");
+    test_str(SpanNs::MS * 1.5, "1.5ms");
+    test_str(SpanNs::MS * 1.2, "1.2ms");
+    test_str(SpanNs::MS * 1.23, "1.23ms");
+    test_str(SpanNs::MS * 1.234, "1.234ms");
+    test_str(SpanNs::MS * 1.2345, "1.2345ms");
+    test_str(SpanNs::MS * 1.23456, "1.23456ms");
+    test_str(SpanNs::MS * 1.234567, "1.234567ms");
+    test_str(SpanNs::MS * 1.034567, "1.034567ms");
+    test_str(SpanNs::MS * 0.5, "500us");
+    test_str(SpanNs::US, "1us");
+    test_str(SpanNs::NS, "1ns");
+    test_str(SpanNs::of_day(12.), "12d");
+    test_str(SpanNs::DAY * 12 + SpanNs::NS, "12d1ns");
+    test_str((SpanNs::DAY * 12 + SpanNs::NS).neg(), "-12d1ns");
+    test_str(SpanNs::DAY * 12 + SpanNs::MS + SpanNs::NS, "12d1.000001ms");
+    test_str(SpanNs::DAY + SpanNs::MS + SpanNs::US, "1d1.001ms");
+    test_str(
+        SpanNs::DAY + SpanNs::MS * 123456 + SpanNs::US,
+        "1d2m3.456001s",
     );
-    assert_eq!(
-        (SpanNs::DAY + SpanNs::MS + SpanNs::US).to_string(),
-        "1d1.001ms"
-    );
-    assert_eq!(
-        (SpanNs::DAY + SpanNs::MS * 123456 + SpanNs::US).to_string(),
-        "1d2m3.456001s"
-    );
-    assert_eq!(
-        (SpanNs::DAY + SpanNs::MS * 123456).to_string(),
-        "1d2m3.456s"
-    );
-    assert_eq!(
-        (SpanNs::HR + SpanNs::US * 123456).to_string(),
-        "1h123.456ms"
-    );
-    assert_eq!(
-        (SpanNs::MIN * 12 + SpanNs::US * 123456).to_string(),
-        "12m123.456ms"
-    );
-    assert_eq!(
-        (SpanNs::MIN * 12 + SpanNs::NS * 123000).to_string(),
-        "12m123us"
-    );
-    assert_eq!(
-        (SpanNs::MIN * 12 + SpanNs::NS * 123001).to_string(),
-        "12m123.001us"
-    );
+    test_str(SpanNs::DAY + SpanNs::MS * 123456, "1d2m3.456s");
+    test_str(SpanNs::HR + SpanNs::US * 123456, "1h123.456ms");
+    test_str(SpanNs::MIN * 12 + SpanNs::US * 123456, "12m123.456ms");
+    test_str(SpanNs::MIN * 12 + SpanNs::NS * 123000, "12m123us");
+    test_str(SpanNs::MIN * 12 + SpanNs::NS * 123001, "12m123.001us");
 }
