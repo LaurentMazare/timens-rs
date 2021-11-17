9cd0d1b3254f5acb2374300ff9cea28aa9d16db3
laurent <laurent.mazare@gmail.com>
2021-11-17 22:00:27 +0000

    Add some date tests.

--- tests/date_tests.rs
+++ tests/date_tests.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use timens::{Date, Month};
 
 #[test]
@@ -7,5 +8,11 @@ fn add_sub() {
     assert_eq!((d + 1).to_string(), "2000-01-02");
     assert_eq!((d - 1).to_string(), "1999-12-31");
     assert_eq!((d + 365).to_string(), "2000-12-31");
-    assert_eq!((d + 366).to_string(), "2001-01-01")
+    assert_eq!((d + 366).to_string(), "2001-01-01");
+    let d = Date::from_str("2021-11-17").unwrap();
+    assert_eq!(d.to_string(), "2021-11-17");
+    for i in -10_000..10_000 {
+        assert_eq!(d - i + i, d);
+        assert_eq!(d + i - i, d);
+    }
 }
